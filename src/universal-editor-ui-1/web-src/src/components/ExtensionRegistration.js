c0417b095a15332d66d00103e8680e2eb67f055c
Talmiz Ahmed <talmiz.tech@gmail.com>
2024-11-04 11:32:04 +0530

    Dialog code

---
+++ src/universal-editor-ui-1/web-src/src/components/ExtensionRegistration.js
@@ -0,0 +1,49 @@
+/*
+Copyright 2024 Adobe
+All Rights Reserved.
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it.
+*/
+
+import {Text} from "@adobe/react-spectrum";
+import {register} from "@adobe/uix-guest";
+import {extensionId} from "./Constants";
+import React, { useEffect, useState } from 'react';
+function ExtensionRegistration() {
+  useEffect(() => {
+      const init = async () => {
+          const registrationConfig = {
+              id: extensionId,
+              methods: {
+                headerMenu: {
+                  getButtons() {
+                    return [
+                      {
+                        id: `${extensionId}-button`,
+                        label: "Export form",
+                        icon: 'ExperienceExport', // Spectrum workflow icon code from https://spectrum.adobe.com/page/icons/
+                        onClick: async () => {
+                          console.log('Button has been pressed.');      
+                          const url = '/#/modal/drop'; // absolute or relative path
+                          guestConnection.host.modal.showUrl({
+                            title: 'Export and Drop In Anywhere: ',
+                            url,
+                          });
+                        },
+                      }
+                    ]
+                  }
+                }
+              }
+          };
+          const guestConnection = await register(registrationConfig);
+      }
+      init().catch(console.error)
+  }, []);
+
+return <Text>IFrame for integration with Host...</Text>
+
+}
+
+export default ExtensionRegistration;
---
+++ src/universal-editor-ui-1/web-src/src/components/Modal.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { attach } from "@adobe/uix-guest";
+import {
+  Provider,
+  Content,
+  defaultTheme,
+  Button,
+  Text
+} from "@adobe/react-spectrum";
+import { extensionId } from "./Constants";
+import Copy from "@spectrum-icons/workflow/Copy";
+
+export default ModalComponent = () => {
+  const [guestConnection, setGuestConnection] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    (async () => {
+      const guestConnection = await attach({ id: extensionId });
+
+      setGuestConnection(guestConnection);
+      setLoading(false)
+    })();
+  }, []);
+
+  const onCloseHandler = () => {
+    guestConnection.host.modal.close();
+  };
+  const onCopyHandler = () => {
+    console.log("copy logic pending")
+  };
+    return (
+        !loading ? <Provider theme={defaultTheme} colorScheme="light">
+            <Content width="100%">
+            Some content of mine
+                <Button variant="accent" onPress={onCopyHandler} position="fixed" bottom="0px" right="100px" >
+                  <Copy />
+                  <Text>Copy to Clipboard</Text>
+                 </Button>
+                <Button variant="primary" onPress={onCloseHandler} position="fixed" bottom="0px" right="8px">
+                    Close
+                 </Button>
+            </Content>
+        </Provider> : <Content>Loading....</Content>
+    );
+ }
+    
+  
